379fe84c80ef05d9715bf6bcc59621dfbd074c28
Dorian <dorian@resalocal.com>
2023-11-12 15:04:49 +0100

    Add alyout grid

---
+++ components/LayoutGrid.vue
@@ -0,0 +1,15 @@
+<template>
+  <main
+    class="layout-grid grid grid-rows-[auto_1fr_auto] grid-cols-[minmax(24px,auto)_minmax(auto,1024px)_minmax(24px,auto)] gap-y-3 min-h-screen"
+  >
+    <slot />
+  </main>
+</template>
+
+<style lang="scss">
+.layout-grid {
+  > * {
+    grid-column: 2;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,9 @@
 <template>
-  <main
-    class="layout grid grid-rows-[auto_1fr_auto] grid-cols-[minmax(24px,auto)_minmax(auto,1024px)_minmax(24px,auto)] gap-y-3 min-h-screen"
-  >
+  <LayoutGrid>
     <Header />
     <div>
       <slot />
     </div>
     <Footer />
-  </main>
+  </LayoutGrid>
 </template>
-
-<style lang="scss">
-.layout {
-  > * {
-    grid-column: 2;
-  }
-}
-</style>
