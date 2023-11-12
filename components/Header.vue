7caee6ed153288ddd6649b3eb26b6d98c34285a9
Dorian <dorian@resalocal.com>
2023-11-12 15:30:31 +0100

    crazy otter

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,5 @@
 <template>
   <header class="flex items-center gap-6">
     <Otter />
-    <h1>Otter</h1>
   </header>
 </template>
--- components/Otter.vue
+++ components/Otter.vue
@@ -1,1 +1,23 @@
-<template><img class="h-16 w-auto" src="otter.gif" /></template>
+<template>
+  <div>
+    <figure class="otter">
+      <h1 class="otter__title"><span class="otter__o">O</span>tter</h1>
+      <img class="otter__gif h-16 w-auto" src="otter.gif" />
+    </figure>
+  </div>
+</template>
+<style lang="scss">
+.otter {
+  position: relative;
+
+  &__gif {
+    position: absolute;
+    bottom: 34px;
+    left: 8px;
+  }
+
+  &__o {
+    font-size: 150px;
+  }
+}
+</style>
