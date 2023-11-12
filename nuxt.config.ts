999f25b5f4b1b2cf70bf7a85c6c9883c9ba25519
Dorian <dorian@resalocal.com>
2023-11-12 14:32:16 +0100

    add baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: {enabled: true},
+  modules: ["@nuxt/ui"],
+  privateRuntimeConfig: {
+    api: {
+      baseURL: "http://localhost:8080",
+    },
+  },
 });
