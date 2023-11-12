de7130aca6e5517ed6801e32a5a58d1beb2f50c7
Dorian <dorian@resalocal.com>
2023-11-12 15:01:43 +0100

    add sass

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <main
+    class="layout grid grid-rows-[auto_1fr_auto] grid-cols-[minmax(24px,auto)_minmax(auto,1024px)_minmax(24px,auto)] gap-y-3 min-h-screen"
+  >
+    <Header />
+    <div>
+      <slot />
+    </div>
+    <Footer />
+  </main>
+</template>
+
+<style lang="scss">
+.layout {
+  > * {
+    grid-column: 2;
+  }
+}
+</style>
